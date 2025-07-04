4a716867adef0670ebfba36648d1eb44f9509c1c
Abhinav Koul <temppgdai2025@gmail.com>
2025-07-04 18:36:42 +0530

    first commit on branch sub

---
+++ sub.py
@@ -0,0 +1,11 @@
+# Ask the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+diff = num1 - num2
+
+# Display the result
+print("The sub is:", diff)
